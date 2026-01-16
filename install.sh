b702d2283ed4db66936fcd32000b89fd092fb53d
Lysander Lyu <lvyc@realbom.com>
2026-01-16 11:04:07 +0800

    install: use override version install

--- install.sh
+++ install.sh
@@ -6,33 +6,25 @@ SCRIPT_PATH="$(realpath "$0")"
 ROOT_DIR="$(dirname "$SCRIPT_PATH")"
 
 if [ "$(uname)" = "Linux" ]; then
-    # First remove the origin profile
-    rm ~/.bashrc
-    rm ~/.profile
-
-    # Then hard link to them back to
-    ln ${ROOT_DIR}/.bashrc ~/
-    ln ${ROOT_DIR}/.profile ~/
+    ln -f ${ROOT_DIR}/.bashrc ${HOME}/
+    ln -f ${ROOT_DIR}/.profile ${HOME}/
 
 elif [ "$(uname)" = "Darwin" ];then
-    # First remove the origin profile
-    rm ~/.zshrc
-    rm ~/.zprofile
-
-    # Then hard link to them back to
-    ln ${ROOT_DIR}/.zshrc ~/
-    ln ${ROOT_DIR}/.profile ~/.zprofile
+    ln -f ${ROOT_DIR}/.zshrc ${HOME}/
+    ln -f ${ROOT_DIR}/.profile ${HOME}/.zprofile
 else
     echo "Other platform"
 fi
 
 # Some general configuration
-rm ~/.bash_aliases
-rm ~/.vimrc
-rm ~/.gitconfig
-rm ~/.config/zathura/zathurarc
+ln -f ${ROOT_DIR}/.bash_aliases ${HOME}/
+ln -f ${ROOT_DIR}/.vimrc ${HOME}/
+ln -f ${ROOT_DIR}/.qt512.env ${HOME}/
+ln -f ${ROOT_DIR}/.gitconfig ${HOME}/
 
-ln ${ROOT_DIR}/.bash_aliases ~/
-ln ${ROOT_DIR}/.vimrc ~/
-ln ${ROOT_DIR}/.gitconfig ~/
-ln ${ROOT_DIR}/.config/zathura/zathurarc ~/.config/zathura/zathurarc
+mkdir -p ${ROOT_DIR}/.config/zathura/ 
+ln -f ${ROOT_DIR}/.config/zathura/zathurarc ${HOME}/.config/zathura/zathurarc
+
+mkdir -p ${HOME}/bin
+ln -f ${ROOT_DIR}/mountSamba.sh ${HOME}/bin/mountSamba.sh
+ln -f ${ROOT_DIR}/lfsChangeRoot.sh ${HOME}/bin/lfsChangeRoot.sh
